5670ed3b62323d9ab4031df6263c46dd628f1b49
ebaran60 <eyupbaran1425@gmail.com>
2023-05-22 15:48:55 +0300

    Kodlar temizlendi ve README klasörü oluşturuldu

---
+++ README.md
@@ -0,0 +1,10 @@
+## **Proje Amacı**
+
+[maestrocr.com](http://maestrocr.com/) ürününe ait blog yazılarının listesini ve her yazıya ait detayı görüntüleyebilmemizi sağlayan uygulama geliştirmek.
+
+## **Proje Nasıl Çalıştırılır**
+
+1 - Bilgisayarımıza Nodejs uygulamasını kuruyoruz
+2 - Android veya Ios işletim sistemli telefonumuzu bilgisayarımıza bağlıyoruz
+3 - Terminalden projemizin bulundu klasöre giriyoruz.
+4 - Terminalimizden "npx react-native run-android" kodunu çalıştırıyoruz.
--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -1,21 +1,18 @@
-import { Pressable, Image, Text, View, ScrollView, useWindowDimensions } from 'react-native'
+import { Pressable, Image, Text, View } from 'react-native'
 import React from 'react'
-import RenderHTML from 'react-native-render-html'
 
 import styles from './CategoryCard.styles'
 
-const CategoryCard = ({banner, title, summary, totalReadingTime, content, onSelect}) => {
-  const { width } = useWindowDimensions();
-  const html = content;
+const CategoryCard = ({banner, title, summary, totalReadingTime, onSelect}) => {
   return (
     <Pressable style={styles.container} onPress={onSelect}>
         <Image source={{uri: banner}} style={styles.image} />
         <View style={styles.textContainer}>
-        <Text style={styles.title}>Title</Text>
-        <Text style={styles.content}>{title}</Text>
-        <Text style={styles.title}>summary</Text>
-        <Text style={styles.content}>{summary}</Text>
-        <Text style={styles.content}>totalReadingTime: {totalReadingTime}</Text>
+          <Text style={styles.title}>Title</Text>
+          <Text style={styles.content}>{title}</Text>
+          <Text style={styles.title}>summary</Text>
+          <Text style={styles.content}>{summary}</Text>
+          <Text style={styles.content}>totalReadingTime: {totalReadingTime}</Text>
         </View>
     </Pressable>
   )
